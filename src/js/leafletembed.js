818f6912c47684acd6766961c7eddc1ccd5cc98d
Marlon B Hernandez <marlonbilbaohernandez@gmail.com>
2018-11-25 22:43:12 -0800

    working range indicator

--- src/js/leafletembed.js
+++ src/js/leafletembed.js
@@ -2,24 +2,65 @@
     
 
         var mymap;
-
+        var fuelLevel;
         var myLat;
         var myLng;
         
         gm.info.getCurrentPosition(processPosition, true)
-        debugger;
 
         function processPosition(position){
             var myLat = position.coords.latitude;
             var myLng = position.coords.longitude;
 
             setMapPosition(myLat, myLng);
+
+
+            gm.info.getVehicleData(getFuelLevel, ['fuel_level']);
+
+          
+            function getFuelLevel(data) {           
+            setVariableRange(data);
+            }
+    
+            function setVariableRange(fuelLevel) {
+                //fuelEconomy varies according to vehicle
+                let fuelEconomy = 100;
+                let fuelPercentage = fuelLevel.fuel_level;
+                let range = fuelLevel.fuel_level * fuelEconomy;
+                let colorWarning;
+
+
+                if (fuelPercentage > 60) {
+                    colorWarning = 'blue'
+                } else if (fuelPercentage < 60 && fuelPercentage > 20) {
+                    colorWarning = 'yellow'
+                } else if (fuelPercentage < 20) {
+                    colorWarning = 'red'
+                }
+
+                //there is probably a problem with resolution
+                //causing this circle to be offcenter
+                // L.circle([myLat+.04, myLng-.08], {radius:range}, {
+                //     color: 'red',
+                //     fillColor: 'red',
+                //     fillOpacity: 0.2
+                //   }).addTo(mymap).bindPopup("I am a circle.");
+
+                L.circle([myLat+.04, myLng-.08], range, {
+                color: colorWarning,
+                fillColor: colorWarning,
+                fillOpacity: 0.2
+                }).addTo(mymap).bindPopup("I am a circle.");
+  
+            }
         }
 
 
+
+      
      
-        function setMapPosition(myLat, myLng) {
-        mymap = L.map('mapid').setView([myLat, myLng], 13);
+        function setMapPosition(incomingLat, incomingLng) {
+        mymap = L.map('mapid').setView([incomingLat, incomingLng], 12);
 
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
@@ -28,12 +69,11 @@
               'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             id: 'mapbox.streets'
           }).addTo(mymap);
-  
 
         }
-
+          
         
-
+        
 
   
         // L.marker([51.5, -0.09]).addTo(mymap)
