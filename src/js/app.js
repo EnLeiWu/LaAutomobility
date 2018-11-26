7afa78f4fee6e9467b265cbdd05c540d07c9e26d
Marlon B Hernandez <marlonbilbaohernandez@gmail.com>
2018-11-25 21:02:41 -0800

    pass vehicle lat/long to maps

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,32 @@
-var useMetricUnits = true
-changeUnits(document.getElementById('unitSwitch'))
+// var useMetricUnits = true
+// changeUnits(document.getElementById('unitSwitch'))
 
-var vinElem = document.getElementById('vin');
-gm.info.getVehicleConfiguration(function(data) {
-  vinElem.innerHTML = gm.info.getVIN();
-});
+// var vinElem = document.getElementById('vin');
+// gm.info.getVehicleConfiguration(function(data) {
+//   vinElem.innerHTML = gm.info.getVIN();
+// });
 
 
-function showSpeed(data) {
-  console.log("showSpeed - metric? " + useMetricUnits)
-  var speed = data.average_speed
-  if ( speed !== undefined ) {
-    var speedText = document.getElementById('speed')
-    speed = useMetricUnits ? speed : Math.round(speed * 0.621)
-    speedText.innerHTML = speed
-  }
-}
 
-function changeUnits(checkbox) {
-  console.log("Changed!" + checkbox.checked)
-  useMetricUnits = checkbox.checked
-  gm.info.getVehicleData(showSpeed, ['average_speed'])
-}
 
-gm.info.watchVehicleData(showSpeed, ['average_speed'])
+// function showSpeed(data) {
+//   console.log("showSpeed - metric? " + useMetricUnits)
+//   var speed = data.average_speed
+//   if ( speed !== undefined ) {
+//     var speedText = document.getElementById('speed')
+//     speed = useMetricUnits ? speed : Math.round(speed * 0.621)
+//     speedText.innerHTML = speed
+//   }
+// }
 
-function tester(){
-  window.location.href = './tester.html';
-}
+// function changeUnits(checkbox) {
+//   console.log("Changed!" + checkbox.checked)
+//   useMetricUnits = checkbox.checked
+//   gm.info.getVehicleData(showSpeed, ['average_speed'])
+// }
+
+// gm.info.watchVehicleData(showSpeed, ['average_speed'])
+
+// function tester(){
+//   window.location.href = './tester.html';
+// }
--- src/js/leafletembed.js
+++ src/js/leafletembed.js
@@ -1,40 +1,64 @@
 
     
 
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        var mymap;
+
+        var myLat;
+        var myLng;
+        
+        gm.info.getCurrentPosition(processPosition, true)
+        debugger;
+
+        function processPosition(position){
+            var myLat = position.coords.latitude;
+            var myLng = position.coords.longitude;
+
+            setMapPosition(myLat, myLng);
+        }
+
+
+     
+        function setMapPosition(myLat, myLng) {
+        mymap = L.map('mapid').setView([myLat, myLng], 13);
 
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-          maxZoom: 18,
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          id: 'mapbox.streets'
-        }).addTo(mymap);
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+              '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+              'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox.streets'
+          }).addTo(mymap);
   
-        L.marker([51.5, -0.09]).addTo(mymap)
-          .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-  
-        L.circle([51.508, -0.11], 500, {
-          color: 'red',
-          fillColor: '#f03',
-          fillOpacity: 0.2
-        }).addTo(mymap).bindPopup("I am a circle.");
-  
-        L.polygon([
-          [51.509, -0.08],
-          [51.503, -0.06],
-          [51.51, -0.047]
-        ]).addTo(mymap).bindPopup("I am a polygon.");
-  
-  
-        var popup = L.popup();
-  
-        function onMapClick(e) {
-          popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(mymap);
+
         }
+
+        
+
+
   
-        mymap.on('click', onMapClick);
+        // L.marker([51.5, -0.09]).addTo(mymap)
+        //   .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
   
+        // L.circle([51.508, -0.11], 500, {
+        //   color: 'red',
+        //   fillColor: '#f03',
+        //   fillOpacity: 0.2
+        // }).addTo(mymap).bindPopup("I am a circle.");
+  
+        // L.polygon([
+        //   [51.509, -0.08],
+        //   [51.503, -0.06],
+        //   [51.51, -0.047]
+        // ]).addTo(mymap).bindPopup("I am a polygon.");
+  
+        // var popup = L.popup();
+  
+        // function onMapClick(e) {
+        //   popup
+        //     .setLatLng(e.latlng)
+        //     .setContent("You clicked the map at " + e.latlng.toString())
+        //     .openOn(mymap);
+        // }
+  
+        // mymap.on('click', onMapClick);
+    
